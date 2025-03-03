c8f7fe8ac788c69601c4efd9aa37d291649a62c6
TahereGmi <mstgholami@gmail.com>
2025-03-03 20:30:23 +0330

    chore: string methods

--- app.py
+++ app.py
@@ -4,13 +4,24 @@ x = 1
 # Primitive variables: string, number, boolean
 course_count = 1000
 x = 3.5
-course_name = "Python programming"
-print(course_name[0])
-print(course_name[:])
-print(course_name[-1:])
-print(len(course_name))
+course_name = "  Python programming"
+# print(course_name[0])
+# print(course_name[:])
+# print(course_name[-1:])
+# print(len(course_name))
 
 first = "Tahereh"
 last = "Gholami"
 full = f"{len(first)} {last}"
 print(full)
+
+# string methods
+print(course_name.upper())
+print(course_name.lower())
+print(course_name.title())
+print(course_name.strip())
+print(course_name.lstrip())
+print(course_name.find("ro"))
+print(course_name.replace("p", "j"))
+print("pro" in course_name)
+print("test" not in course_name)
