5a9fa733291a9046ce2758e6ff1d868b51d5038b
TahereGmi <mstgholami@gmail.com>
2025-05-21 19:05:07 +0330

    feat: using polymorphism

--- app.py
+++ app.py
@@ -805,5 +805,23 @@ class NetworkStream(Stream):
     def read(self):
         print("Memory Stream")
 
+# ************** Polymorphism -> Many Forms
+class UIControl(ABC):
+    @abstractmethod
+    def draw(self):
+        pass
+class TextBox(UIControl):
+    def draw(self):
+        print("TextBox")
 
+class DropDownList(UIControl):
+    def draw(self):
+        print("DropDownList")
+    
+def draw(controls):
+    for control in controls:
+        control.draw()
 
+ddl = DropDownList()
+textbox = TextBox()
+draw([ddl, textbox])
