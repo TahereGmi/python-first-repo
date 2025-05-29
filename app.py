51d8752c78f352bfe7f37b70ed2e3ea9414cd7eb
TahereGmi <mstgholami@gmail.com>
2025-05-29 14:33:08 +0330

    feat: working eith Paths

--- app.py
+++ app.py
@@ -951,3 +951,17 @@ sales.calc_shipping()
 # If an object is passed (like a module, class, or instance), 
 # it lists the attributes and methods of that object.
 print(dir(sales))
+
+# ************ Working with Paths *************
+from pathlib import Path
+
+path = Path("ecommerce/__init__.py")
+path.exists()
+path.is_file()
+path.is_dir()
+print(path.name)
+print(path.stem)
+print(path.suffix)
+print(path.parent)
+path = path.with_suffix(".txt")
+print(path)
